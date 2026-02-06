f64ca5dc982fc12a421da721dbea7b991ca5b239
Rufat Ismayilov <ismayilovr75@gmail.com>
2026-02-06 16:22:58 +0400

    TestRunner and DriverManager added

---
+++ src/test/java/core/DriverManager.java
@@ -0,0 +1,19 @@
+package core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverManager {
+    public static WebDriver createChromeDriver(boolean headless) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        if (headless) {
+            options.addArguments("--headless=new");
+        }
+        options.addArguments("--disable-notifications");
+        WebDriver driver = new ChromeDriver(options);
+        return driver;
+    };
+}
