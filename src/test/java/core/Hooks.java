79b0515d1ef75bfa52ea078ab1df36cb8296dd8a
Rufat Ismayilov <ismayilovr75@gmail.com>
2026-02-06 16:57:36 +0400

    Hooks and Test Cases added

---
+++ src/test/java/core/Hooks.java
@@ -0,0 +1,26 @@
+package core;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
